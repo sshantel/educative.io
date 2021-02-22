6f8741ec1b0cd8ec9a1bc0388d324c6b21c5b311
Chantel Yip <chantelyip422@gmail.com>
2021-02-21 16:48:24 -0800

    added testcases

---
+++ grokking_the_coding_interview/maximum_sum_subarray_of_size_k.py
@@ -0,0 +1,7 @@
+"""
+Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
+>>> max_sub_array_of_size_k([2, 1, 5, 1, 3, 2], 3)
+9
+>>> max_sub_array_of_size_k([[2, 3, 4, 1, 5], 2)
+7
+"""
