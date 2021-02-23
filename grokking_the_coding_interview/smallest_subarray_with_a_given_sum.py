2c39b49b83fc69a3d41b968407272d0545a4d8e2
Chantel Yip <chantelyip422@gmail.com>
2021-02-22 21:44:40 -0800

    smallest subarray with given sum

---
+++ grokking_the_coding_interview/smallest_subarray_with_a_given_sum.py
@@ -0,0 +1,35 @@
+"""
+Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
+"""
+#Time Complexity #
+#The time complexity of the above algorithm will be O(N)O(N). The outer for loop runs for all elements, and the inner while loop processes each element only once; therefore, the time complexity of the algorithm will be O(N+N)O(N+N), which is asymptotically equivalent to O(N)O(N).
+
+#Space Complexity #
+#The algorithm runs in constant space O(1)O(1).
+import math
+
+
+def smallest_subarray_with_given_sum(s, arr):
+  window_sum = 0
+  min_length = math.inf
+  window_start = 0
+
+  for window_end in range(len(arr)):
+    window_sum += arr[window_end]  # add the next element
+    # shrink the window as small as possible until the 'window_sum' is smaller than 's'
+    while window_sum >= s:
+      min_length = min(min_length, window_end - window_start + 1)
+      window_sum -= arr[window_start]
+      window_start += 1
+  if min_length == math.inf:
+    return 0
+  return min_length
+
+
+def main():
+  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2])))
+  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])))
+  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])))
+
+
+main()
