37b5e94c53f94db06c3609a48ba0b148ae9e43bf
Chantel Yip <chantelyip422@gmail.com>
2021-02-22 20:38:35 -0800

    added brute force method

--- grokking_the_coding_interview/maximum_sum_subarray_of_size_k.py
+++ grokking_the_coding_interview/maximum_sum_subarray_of_size_k.py
@@ -1,7 +1,28 @@
 """
-Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
+Given an array of positive numbers and a positive number ‘k,’ 
+find the maximum sum of any contiguous subarray of size ‘k’.
+
 >>> max_sub_array_of_size_k([2, 1, 5, 1, 3, 2], 3)
 9
 >>> max_sub_array_of_size_k([[2, 3, 4, 1, 5], 2)
 7
+
 """
+#bruteforce 
+def max_sub_array_of_size_k(k, arr):
+  max_sum = 0
+  window_sum = 0 
+  for i in range(len(arr) - k + 1):  
+    window_sum = 0
+    for j in range(i, i+k):
+      window_sum += arr[j]
+    max_sum = max(max_sum, window_sum)
+  return max_sum
+
+
+def main():
+  print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
+  print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])))
+
+
+main()
